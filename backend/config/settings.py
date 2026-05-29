fb06135ef29cf5123cb4a1ff662ce9860ef08ea4
Yugendran-2023 <yugendranck1973@gmail.com>
2026-05-29 22:41:36 +0530

    Backend deployment setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
