6f9b6a9263d9dc40e84468974dd43b8875d025f3
Yugendran-2023 <yugendranck1973@gmail.com>
2026-05-29 22:55:17 +0530

    Updated production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
 
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://breathe-esg-ingestion-platform-w4ol.onrender.com/api/',
 
 });
 
